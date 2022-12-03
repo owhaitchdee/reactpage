b26d97ca8826bfb5a85cf47f98b2806e9bd5eafc
owhaitchdee <108674156+owhaitchdee@users.noreply.github.com>
2022-12-03 03:21:30 -0500

    new url

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,8 +11,8 @@ function About(){
                 <h3> My Profile </h3>    
                 {['Dark'].map((variant) => (
                     <Card key={variant} text={variant.toLowerCase() === 'light' ? 'dark' : 'white'} style={{ width: 'auto', backgroundColor:'black', opacity:'80%', padding:'20px 40px', }} className="mt-4">                   
-                    <p className='par' style={{fontSize:'15px'}}> I am passionate about being in the business of transformation.</p>
-                    <p className='par'style={{fontSize:'13px'}}>For the past 20 years, I have been with a team who help clients improve the way they operate and manage their businesses through innovative products and latest technologies. This has helped me develop my business and technical expertise in various areas: business analysis, project management, product designing and development, quality management, marketing and business development.<br/><br/>
+                    <p className='par' style={{fontSize:'17px'}}> I am passionate about being in the business of transformation.</p>
+                    <p className='par'style={{fontSize:'15px'}}>For the past 20 years, I have been with a team who help clients improve the way they operate and manage their businesses through innovative products and latest technologies. This has helped me develop my business and technical expertise in various areas: business analysis, project management, product designing and development, quality management, marketing and business development.<br/><br/>
                         The sense of fulfillment comes from knowing that the software solutions we provide have transformed our clients' businesses and helped them stay ahead of competition.<br/><br/>
                         My goal is to bring the transformation we do to greater levels by creating web-enabled businesses and changing industries in a way that challenges what others know as impossible.</p>
                         <Image src={profile} className='mx-auto d-block d-flex' style={{ width:'120px', verticalAlign:'middle', borderRadius:'50%'}}/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ function Home(){
     return (
         <div>
           <BannerCarou/>
-          <Student/>
         </div>
     )
 }
